70af074ca761df10f5c82b194f7393d43537b4d3
Ichiro Nakagawa <ichiron@outlook.com>
2026-07-31 18:45:53 +0900

    公開完了

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import cesium from 'vite-plugin-cesium';
 export default defineConfig({
-  plugins: [cesium()],
+  plugins: [
+    cesium({
+      cesiumBaseUrl: 'cesium/',
+    }),
+  ],
   base: '/aeroweather-insight/',
 });
